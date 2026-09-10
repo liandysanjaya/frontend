c83c562d17cc6ad57902d79e43989b3c8efff98f
liandysanjaya <12410209@student.unklab.ac.id>
2026-09-10 11:55:20 +0800

    Update Navbar component with title and menu items

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
+  const menuItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Portfolio', href: '/portfolio' },
+    { label: 'Blog', href: '/blog' },
+  ];
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      <Navbar title="Liandy Sanjaya" items={menuItems} />
       <Hero />
       <Features />
       <Footer />
-    </div>
+    <div/>
   );
 }
-
-
 
 export default App;
